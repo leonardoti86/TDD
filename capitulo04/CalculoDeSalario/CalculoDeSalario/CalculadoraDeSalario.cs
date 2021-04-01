c8b129d208a04144f7af66dca5d1d2f4b8f426c6
leonardoti86 <leonardoti86@gmail.com>
2021-03-31 23:51:07 -0300

    implementação e teste

---
+++ capitulo04/CalculoDeSalario/CalculoDeSalario/CalculadoraDeSalario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculoDeSalario
+{
+    public class CalculadoraDeSalario
+    {
+        public double CalculaSalario(Funcionario funcionario)
+        {
+            if (funcionario.Cargo.Equals(Cargo.DESENVOLVEDOR))
+            {
+                if (funcionario.Salario > 3000)
+                    return funcionario.Salario * 0.8;
+
+                return funcionario.Salario * 0.9;
+            }
+
+            return 425.0;
+
+        }
+    }
+}
---
+++ capitulo04/CalculoDeSalario/CalculoDeSalario/Funcionario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculoDeSalario
+{
+    public enum Cargo
+    {
+        DESENVOLVEDOR,
+        DBA,
+        TESTADOR
+    }
+
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+        public Cargo Cargo { get; set; }
+
+        public Funcionario(string nome, double salario, Cargo cargo)
+        {
+            Nome = nome;
+            Salario = salario;
+            Cargo = cargo;
+        }
+    }
+}
