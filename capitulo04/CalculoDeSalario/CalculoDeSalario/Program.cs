45558a363016d5ffe62ca73e03f9d0261888d4d3
leonardoti86 <leonardoti86@gmail.com>
2021-03-31 22:30:38 -0300

    criação projeto CalculoDeSalario

---
+++ capitulo04/CalculoDeSalario/CalculoDeSalario/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculoDeSalario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
