53f39e11ee8b53040867be2e028aee2dd0a88322
leonardoti86 <leonardoti86@gmail.com>
2021-04-10 22:59:12 -0300

    commit inicial

---
+++ capitulo05/LojaVirtual/LojaVirtual/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LojaVirtual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
