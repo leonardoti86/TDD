3761f9f2c939687b1e08ddaac1f78b425dd57534
leonardoti86 <leonardoti86@gmail.com>
2021-04-10 23:15:47 -0300

    criação projeto de teste

---
+++ capitulo05/LojaVirtual/LojaVirtual/CarrinhoDeCompras.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LojaVirtual
+{
+    public class CarrinhoDeCompras
+    {
+        public IList<Item> Itens { get; private set; }
+
+        public CarrinhoDeCompras()
+        {
+            Itens = new List<Item>();
+        }
+
+        public void Adiciona(Item item)
+        {
+            Itens.Add(item);
+        }
+    }
+}
