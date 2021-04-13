7005a95cb6493f1fed68f88cb1643ec9fec4f2f9
leonardoti86 <leonardoti86@gmail.com>
2021-04-12 23:45:25 -0300

    Test Data Builder

---
+++ capitulo05/LojaVirtual/LojaVirtual/CarrinhoDeComprasBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LojaVirtual
+{
+    /// <summary>
+    /// Test Data Builder. 
+    /// Design Pattern Builder.
+    /// A idéia é facilitar o processo de criação de objetos que serão usados em cenarios de teste.
+    /// </summary>
+    public class CarrinhoDeComprasBuilder
+    {
+        public CarrinhoDeCompras carrinho;
+
+        public CarrinhoDeComprasBuilder()
+        {
+            carrinho = new CarrinhoDeCompras();
+        }
+        public CarrinhoDeCompras Cria()
+        {
+            return carrinho;
+        }
+
+        public CarrinhoDeComprasBuilder ComItens(params double[] valores) 
+        {
+            foreach (double valor in valores)
+            {
+                carrinho.Adiciona(new Item("item", 1, valor));
+            }
+
+            return this;
+        }
+
+    }
+}
--- capitulo05/LojaVirtual/LojaVirtualTest/CarrinhoDeComprasTest.cs
+++ capitulo05/LojaVirtual/LojaVirtualTest/CarrinhoDeComprasTest.cs
@@ -14,25 +14,33 @@ namespace LojaVirtualTest
         [TestMethod]
         public void DeveRetornarZeroSeCarrinhoVazio()
         {
-            var carrinho = Inicializa();
+            //var carrinho = Inicializa();
+            var carrinho = new CarrinhoDeComprasBuilder().Cria();
+            
             Assert.AreEqual(0.0, carrinho.MaiorValor(), 0.0001);
         }
 
         [TestMethod]
         public void DeveRetornarValorDoItemSeCarrinhoCom1Elemento()
         {
-            var carrinho = Inicializa();
-            carrinho.Adiciona(new Item("Geladeira", 1, 900.0));
+            //var carrinho = Inicializa();
+            //carrinho.Adiciona(new Item("Geladeira", 1, 900.0));
+            
+            var carrinho = new CarrinhoDeComprasBuilder().ComItens(900.0).Cria();
+
             Assert.AreEqual(900.0, carrinho.MaiorValor(), 0.0001);
         }
 
         [TestMethod]
         public void DeveRetornarMaiorValorSeCarrinhoContemMuitosElementos()
         {
-            var carrinho = Inicializa();
-            carrinho.Adiciona(new Item("Geladeira", 1, 900.0));
-            carrinho.Adiciona(new Item("Fogão", 1, 1500.0));
-            carrinho.Adiciona(new Item("Maquina de lavar", 1, 750));
+            //var carrinho = Inicializa();
+            //carrinho.Adiciona(new Item("Geladeira", 1, 900.0));
+            //carrinho.Adiciona(new Item("Fogão", 1, 1500.0));
+            //carrinho.Adiciona(new Item("Maquina de lavar", 1, 750));
+
+            var carrinho = new CarrinhoDeComprasBuilder().ComItens(900.0, 1500.0, 750).Cria();
+
             Assert.AreEqual(1500.0, carrinho.MaiorValor(), 0.0001);
         }
     }
