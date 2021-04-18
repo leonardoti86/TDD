3105e08466ecc4e4ddd5a0cc327669a49344a81d
leonardoti86 <leonardoti86@gmail.com>
2021-04-17 23:52:28 -0300

    criação projeto

---
+++ capitulo08/NotaFiscal/NotaFiscal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NotaFiscal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
