714a3350818ec887a8ce20548f05885a1ca45f79
leonardoti86 <leonardoti86@gmail.com>
2021-04-23 23:28:01 -0300

    criação projeto ProcessadorDeBoletosTest

---
+++ capitulo09/ProcessadorBoleto/ProcessadorBoletoTest/ProcessadorDeBoletosTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProcessadorBoletoTest
+{
+    [TestClass]
+    public class ProcessadorDeBoletosTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
